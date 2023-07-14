375c79f7afb7c94c36b4dd06079ffc8bf3e89d39
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-14 11:30:40 +0200

    Chercher l’intrus

---
+++ air03.rb
@@ -0,0 +1,37 @@
+#Fonction utilisées
+def check_errors_arguments(arguments)
+	return true if arguments.length <= 2
+end
+
+def find_intruder(array)
+	n = array.length
+	intruders = []
+
+	n.times do |i|
+		#On compte le nombre d'occurences dans le tableau passé en argument ; Si == 1, on insère la valeur dans le tableau intruders
+		if array.count(array[i]) == 1
+			intruders << array[i]
+		end
+	end
+	
+	#Cas où il y a que des paires
+	if intruders.length == 0
+		puts "error : only pairs"
+		exit
+	end
+	return intruders
+end
+
+#Partie 1 : Gestion d'erreur
+if check_errors_arguments(ARGV)
+	puts "error"
+	exit
+end
+
+#Partie 2 : Parsing
+
+#Partie 3 : Résolution
+result = find_intruder(ARGV)
+
+#Partie 4 : Affichage
+puts result
