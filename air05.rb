66838cd50ede871ca537950181841a59689b8f61
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-07-18 11:26:38 +0200

    Sur chacun d’entre eux

--- air05.rb
+++ air05.rb
@@ -1,4 +1,21 @@
 #Fonction utilisées
+def check_errors(arguments)
+	if arguments.length < 2
+		puts "error"
+		exit
+	end
+
+	if !arguments.last.include?("+") && !arguments.last.include?("-")
+		puts "error"
+		exit
+	end
+
+	if arguments.any? { |element| element.match(/[a-zA-Z]/) }
+		puts "error"
+		exit
+	end
+end
+
 def substract_or_add_to_array(array, operator_sign, operator_number)
 	#On convertit le tableau en entrée et le nombre pour l'opération en Integer
 	array.map!(&:to_i)
@@ -19,27 +36,13 @@ def substract_or_add_to_array(array, operator_sign, operator_number)
 end
 
 #Partie 1 : Gestion d'erreur
-if ARGV.length < 2
-	puts "error"
-	exit
-end
-
-if !ARGV.last.include?("+") && !ARGV.last.include?("-")
-	puts "error"
-	exit
-end
-
-if ARGV.any? { |element| element.match(/[a-zA-Z]/) }
-	puts "error"
-	exit
-end
+check_errors(ARGV)
 
 #Partie 2 : Parsing
 operator = ARGV.last
 operator_sign = operator[0]
 operator_number = operator[1]
-#On retire la dernière valeur du tableau avant d'effecter les additions/soustractions
-ARGV.pop
+ARGV.pop #On retire la dernière valeur du tableau avant d'effecter les additions/soustractions
 
 #Partie 3 : Résolution
 result = substract_or_add_to_array(ARGV, operator_sign, operator_number)
