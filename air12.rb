7bbfeefa1ff631ec4e35064bde145f5f87ecd225
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-08-02 13:29:22 +0200

    Le roi des tris

---
+++ air12.rb
@@ -0,0 +1,36 @@
+#Fonction utilisées
+def check_errors(arguments)
+	if arguments.any? { |element| element.match(/[a-zA-Z]/) }
+		puts "error"
+		exit
+	end
+end
+
+def my_partition(array, pivot)
+	left_array, right_array = [], []
+	array.each do |element|
+		if element < pivot
+			left_array << element
+		elsif element >= pivot
+			right_array << element
+		end
+	end
+	return left_array, right_array
+end
+
+def my_quick_sort(array) #Utilisation de la fonction récursive
+	array.map!(&:to_i)
+	pivot = array.sample
+	return array if array.length <= 1
+	left, right = my_partition(array, pivot)
+	my_quick_sort(left) + my_quick_sort(right)
+end
+
+#Partie 1 : Gestion d'erreur
+check_errors(ARGV)
+
+#Partie 3 : Résolution
+result = my_quick_sort(ARGV)
+
+#Partie 4 : Affichage
+print result
